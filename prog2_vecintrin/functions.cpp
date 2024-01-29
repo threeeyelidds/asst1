e44e6a928b41987bc64bd8b894a3e1c80c591ac0
threeeyelidds <youqunew@gmail.com>
2024-01-28 22:24:16 -0500

    debug

--- prog2_vecintrin/functions.cpp
+++ prog2_vecintrin/functions.cpp
@@ -90,7 +90,7 @@ void clampedExpVector(float* values, int* exponents, float* output, int N) {
     zeroInt = _cmu418_vset_int(0);
 
     for (int i = 0; i < N; i += VECTOR_WIDTH) {
-        maskAll = _cmu418_init_ones();
+        maskAll = _cmu418_init_ones(0);
 
         _cmu418_vload_float(x, values + i, maskAll);
         _cmu418_vload_int(y, exponents + i, maskAll);
