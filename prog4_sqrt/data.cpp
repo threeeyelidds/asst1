86054eab49bc6ebe08f6c7b5f5cf490cf83338b0
threeeyelidds <youqunew@gmail.com>
2024-01-29 22:59:09 -0500

    data

--- prog4_sqrt/data.cpp
+++ prog4_sqrt/data.cpp
@@ -22,11 +22,7 @@ void initBad(float *values, int N) {
     for (int i=0; i<N; i++)
     {
         // Todo: Choose values
-        if (i<N/2) {
-            values[i]=2.999f;
-        } else {
-            values[i] = 1.0f;
-        }
+        values[i] = 1.0f;
     }
 }
 
